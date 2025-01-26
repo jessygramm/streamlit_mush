552d5c8f4b9eed8e29c73204b19df6c4a9049211
jess <gerstein.jessica@gmail.com>
2025-01-26 22:44:07 +0100

    change dir

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,29 +54,29 @@ if page == pages[5]:
     def prediction(option_eff):
         if option_eff == 'Image 1':
             img = 'test.png'
-            grad = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test1_gradcam.png'
-            sh = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test1_shap.png'
+            grad = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test1_gradcam.png'
+            sh = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test1_shap.png'
         elif option_eff == 'Image 2':
             img = 'test2.jpg'
-            grad = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test2_gradcam.png'
-            sh = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test2_shap.png'
+            grad = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test2_gradcam.png'
+            sh = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test2_shap.png'
         elif option_eff == 'Image 3':
             img = 'test3.jpg'
-            grad = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test3_gradcam.png'
-            sh = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test3_shap.png'
+            grad = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test3_gradcam.png'
+            sh = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test3_shap.png'
         elif option_eff == 'Image 4':
             img = 'test4.jpg'
-            grad = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test3_gradcam.png'
-            sh = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/eff_test3_shap.png'
+            grad = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test3_gradcam.png'
+            sh = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/eff_test3_shap.png'
         return img, grad, sh
 
-    model_eff = tf.keras.models.load_model('/home/jess/PycharmProjects/mush_streamlit/model/tuned_efficientnet_model.keras')
+    model_eff = tf.keras.models.load_model('/home/jess/PycharmProjects/streamlit_mush/model/tuned_efficientnet_model.keras')
     img, grad, sh = prediction(option_eff)
-    image_path = '/home/jess/PycharmProjects/mush_streamlit/test/' + img
+    image_path = '/home/jess/PycharmProjects/streamlit_mush/test/' + img
     image = Image.open(image_path).resize((224, 224)).convert('RGB')
 
     predicted_class = np.argmax(model_eff.predict(np.array([image])))
-    class_names = sorted(os.listdir('/home/jess/PycharmProjects/mush_streamlit/dataset'))
+    class_names = sorted(os.listdir('/home/jess/PycharmProjects/streamlit_mush/dataset'))
     predicted_class_name = class_names[predicted_class]
     st.image(image)
     # Print the prediction
@@ -102,29 +102,29 @@ if page == pages[6]:
     def prediction(option_vit):
         if option_vit == 'Image 1':
             img = 'test.png'
-            sha = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test1_shap.png'
-            cap = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test1_captum.png'
+            sha = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test1_shap.png'
+            cap = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test1_captum.png'
         elif option_vit == 'Image 2':
             img = 'test2.jpg'
-            sha = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test2_shap.png'
-            cap = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test2_captum.png'
+            sha = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test2_shap.png'
+            cap = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test2_captum.png'
         elif option_vit == 'Image 3':
             img = 'test3.jpg'
-            sha = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test3_shap.png'
-            cap = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test3_captum.png'
+            sha = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test3_shap.png'
+            cap = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test3_captum.png'
         elif option_vit == 'Image 4':
             img = 'test4.jpg'
-            sha = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test4_shap.png'
-            cap = '/home/jess/PycharmProjects/mush_streamlit/imag_pred/vit_test4_captum.png'
+            sha = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test4_shap.png'
+            cap = '/home/jess/PycharmProjects/streamlit_mush/imag_pred/vit_test4_captum.png'
         return img, sha, cap
 
-    save_path = '/home/jess/PycharmProjects/mush_streamlit/model'
+    save_path = '/home/jess/PycharmProjects/streamlit_mush/model'
     model_save_path = os.path.join(save_path, "vit_model")
 
     feature_extractor = transformers.ViTFeatureExtractor.from_pretrained(model_save_path)
     model_vit = transformers.ViTForImageClassification.from_pretrained(model_save_path)
     img, sha, cap = prediction(option_vit)
-    image_path = '/home/jess/PycharmProjects/mush_streamlit/test/' + img
+    image_path = '/home/jess/PycharmProjects/streamlit_mush/test/' + img
     image = Image.open(image_path).resize((224, 224)).convert('RGB')
 
     # Preprocess the image
@@ -137,7 +137,7 @@ if page == pages[6]:
     predicted_class_index = logits.argmax(-1).item()
 
     # Get the predicted class label
-    class_names = sorted(os.listdir('/home/jess/PycharmProjects/mush_streamlit/dataset'))
+    class_names = sorted(os.listdir('/home/jess/PycharmProjects/streamlit_mush/dataset'))
     predicted_class_label = class_names[predicted_class_index]
     predicted_class_name = class_names[predicted_class_index]
     st.image(image)
